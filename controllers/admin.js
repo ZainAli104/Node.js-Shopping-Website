9b81edf0d745155c06781d4bdbd8440f4ddcf175
ZainAli <chzainali6363@gmail.com>
2024-02-09 17:39:08 +0500

    complete✨ => advance authentication module

--- controllers/admin.js
+++ controllers/admin.js
@@ -62,6 +62,10 @@ exports.postEditProduct = (req, res, next) => {
 
     Product.findById(prodId)
         .then(product => {
+            if (product.userId.toString() !== req.user._id.toString()) {
+                return res.redirect('/')
+            }
+
             product.title = updatedTitle;
             product.price = updatedPrice;
             product.description = updatedDesc;
@@ -76,7 +80,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.find()
+    Product.find({
+        userId: req.user._id
+    })
         // .select('title price -_id')
         // .populate('userId', 'name')
         .then(products => {
@@ -92,7 +98,10 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findByIdAndRemove(prodId)
+    Product.deleteOne({
+        _id: prodId,
+        userId: req.user._id
+    })
         .then(() => {
             console.log('DESTROYED PRODUCT');
             res.redirect('/admin/products');
